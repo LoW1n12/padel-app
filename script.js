27c4eef279857396b3ea4c89a99ce6b88d82863c
Ivan Ivanv <low1n@mail.ru>
2025-08-07 17:14:57 +0300

    feat: Implement calendar view and detail modal

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const API_BASE_URL = "https://shaky-rice-tell.loca.lt";
+    const API_BASE_URL = "https://few-flies-jam.loca.lt";
     const CALENDAR_DAYS_TO_SHOW = 20;
     const tg = window.Telegram.WebApp;
 
